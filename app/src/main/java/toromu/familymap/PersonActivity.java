27aae9711eeb84bc638369f8e6848b13fd67f8a8
AA <austinahlstromwork@gmail.com>
2016-12-06 08:31:42 -0700

    deleting everything

--- app/src/main/java/toromu/familymap/PersonActivity.java
+++
@@ -1,8 +0,0 @@
-package toromu.familymap;
-
-/**
- * Person Activity
- * Created by Austin on 05-Dec-16.
- */
-public class PersonActivity {
-}
--- app/src/main/java/toromu/familymap/SearchActivity.java
+++
@@ -1,8 +0,0 @@
-package toromu.familymap;
-
-/**
- * Search Activity
- * Created by Austin on 05-Dec-16.
- */
-public class SearchActivity {
-}
